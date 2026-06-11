63107b2ed7950d66321fd18502189993f9de0cc2
mexditya <mexditya@gmail.com>
2026-06-11 23:05:39 +0530

    Completed Lecture 4 - If Else

---
+++ L4 - If Else/Questions/EvenOdd.cpp
@@ -0,0 +1,17 @@
+//WAP to check if even or odd.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter the number: ";
+    cin >> num;
+
+    if(num % 2 == 0) 
+        cout << "Even" << endl;
+    else 
+        cout << "Odd" << endl;
+    
+    return 0;
+}
---
+++ L4 - If Else/Questions/VowelConsonant.cpp
@@ -0,0 +1,18 @@
+//WAP to check if the character is vowel or consonant
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    cout << "Enter the character: ";
+    cin >> c;
+
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+       c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+       cout << "Vowel" << endl;
+
+    else 
+        cout << "Consonant" << endl;
+    return 0;
+}
