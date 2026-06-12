dd581bb2b0a2825609188d38019d208b1eb0e4d7
mexditya <mexditya@gmail.com>
2026-06-12 12:06:28 +0530

    Completed Lecture 3 - Introduction to C++

---
+++ L003 - Introduction/Questions/Two_Sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    sum = a + b;
+
+    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
+
+    return 0;
+
+}
